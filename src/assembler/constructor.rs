4476f0ce39e1448ab75d466ca748e85d4145b422
Lightura3000 <134639198+Lightura3000@users.noreply.github.com>
2025-01-26 10:50:04 +0100

    Moved unsigned newtypes to their own file

--- src/assembler/constructor.rs
+++ src/assembler/constructor.rs
@@ -1,65 +1,6 @@
 use either::Either::{self, Left, Right};
-use crate::assembler::register::Register;
-
-#[derive(Debug, Copy, Clone)]
-pub struct U2(u8);
-
-#[allow(dead_code)]
-impl U2 {
-    pub const MAX: u8 = 4;
-
-    pub fn new(value: u8) -> Option<U2> {
-        if value < 4 {
-            Some( U2(value))
-        } else {
-            None
-        }
-    }
-
-    pub fn get(&self) -> u8 {
-        self.0
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-pub struct U3(u8);
-
-#[allow(dead_code)]
-impl U3 {
-    pub const MAX: u8 = 8;
-
-    pub fn new(value: u8) -> Option<U3> {
-        if value < 8 {
-            Some( U3(value))
-        } else {
-            None
-        }
-    }
-
-    pub fn get(&self) -> u8 {
-        self.0
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-pub struct U6(u8);
-
-#[allow(dead_code)]
-impl U6 {
-    pub const MAX: u8 = 64;
-
-    pub fn new(value: u8) -> Option<U6> {
-        if value < 64 {
-            Some( U6(value))
-        } else {
-            None
-        }
-    }
-
-    pub fn get(&self) -> u8 {
-        self.0
-    }
-}
+use super::register::Register;
+use super::unsigned_newtypes::{U2, U3, U6};
 
 #[repr(u32)]
 enum InstrType {
