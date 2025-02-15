38e0309afcd7b53b4b7723123830259bc50ed662
Lightura3000 <134639198+Lightura3000@users.noreply.github.com>
2025-02-15 20:45:02 +0100

    Removed Errors that weren't being constructed

--- src/assembler/assembly_error.rs
+++ src/assembler/assembly_error.rs
@@ -6,9 +6,6 @@ pub struct AssemblyError {
 }
 
 pub enum AssemblyErrorVariant {
-    NeedsStartingOpcode,
-    ParamAmount { expected: usize, got: usize },
-    ParamTypes,
     ImmediateTooLarge { max: u16, got: u16 },
     NoLabelFound { name: String },
     OffsetTooLarge { limit: i32, required: i64 },
@@ -19,9 +16,6 @@ pub enum AssemblyErrorVariant {
 impl Display for AssemblyError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let str = match &self.variant {
-            AssemblyErrorVariant::NeedsStartingOpcode => "Line needs to start with an opcode".to_string(),
-            AssemblyErrorVariant::ParamAmount { expected, got } => format!("Expected {} parameters, got {}", expected, got),
-            AssemblyErrorVariant::ParamTypes => "Arguments have invalid types".to_string(),
             AssemblyErrorVariant::ImmediateTooLarge { max, got } => format!("Immediate is too large. Maximum is {} but got {}", max, got),
             AssemblyErrorVariant::NoLabelFound { name } => format!("No label named {} found", name),
             AssemblyErrorVariant::OffsetTooLarge { limit, required } => format!("Offset is too large. Required {} but limit is {}", required, limit),
